bfe75f278e69b5600128cf7aea6820c158884920
Stef-GMS <stephanie@GeekMeSpeak.com>
2025-03-30 15:40:25 -0700

    Initial setup

---
+++ lib/constants/globals.dart
@@ -0,0 +1,23 @@
+late String deviceSize;
+late double gameWidth;
+late double gameHeight;
+
+class Globals {
+  Globals._();
+
+  // static const String backgroundParallaxSprite1 = 'background.png';
+  static const String backgroundParallaxSprite1 = 'background1png';
+  static const String backgroundParallaxSprite2 = 'background2.png';
+  static const String zombieSpriteSheet = 'zombie_spritesheet.png';
+  static const String zombieIdle = 'zombieIdle.png';
+  static const String zombieMove1 = 'zombieMove1.png';
+  static const String zombieMove2 = 'zombieMove2.png';
+  static const String zombieMove3 = 'zombieMove3.png';
+  static const String catSprite = 'cat.png';
+  static const String enemySprite = 'enemy.png';
+  static const String mainMenuSprite = 'title_page.png';
+  static const String youLoseSprite = 'you_lose.png';
+  static const String youWinSprite = 'you_win.png';
+  static const String hitCatSound = 'hitCat.wav';
+  static const String hitEnemySound = 'hitCatLady.wav';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:window_manager/window_manager.dart';
+import 'package:zombie_conga_flame/app/app.dart';
+import 'package:zombie_conga_flame/constants/globals.dart';
+
+void main() async {
+  // Check to see if device is Desktop and if so then set window sizes and title
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+
+    await windowManager.setMinimumSize(const Size(1024, 578)); // 768 doesn't give 16/9 ratio
+    await windowManager.setMaximumSize(const Size(2048, 1156)); // 1536 doesn't give 16/9 ratio
+    await windowManager.setSize(const Size(1024, 578)); // 768 doesn't give 16/9 ratio
+    await windowManager.setAspectRatio(16.0 / 9.0); // aspect ratio supported, 16:9 (1.77)
+    await windowManager.setMaximizable(false);
+
+    await windowManager.setTitle('Zombie Conga'); //Window title
+  }
+
+  runApp(
+    const MainApp(),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,7 +31,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Determine size of screen
+
+    // Get width of device.  Use longestSide for landscape apps, shortestSide for portrait.
+    gameWidth = MediaQuery.of(context).size.longestSide;
+
+    // Get height device.  Use shortestSide for landscape apps, longestSide for portrait.
+    gameHeight = MediaQuery.of(context).size.shortestSide;
+
+    // Set deviceSize based on width.  This is used for font scaling.
+    deviceSize = gameWidth >= 1024
+        ? 'extra large'
+        : gameWidth >= 700
+            ? 'large'
+            : gameWidth > 380
+                ? 'medium'
+                : 'small';
+
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
